cefc650ffd58f23fa3d0eba11443f21b01b42ed0
TheSmokie <jaywalls21@gmail.com>
2021-07-17 14:34:17 -0600

    added SplitContainerItem

--- Craft Code/BankLib.cs
+++ Craft Code/BankLib.cs
@@ -28,6 +28,30 @@ namespace BankLib
             catch { }
         }
 
+        public static void SplitContainerItem(String ItemName, Int32 SplitAMount)
+        {
+            try
+            {
+                for (Int32 Bag = 0; Bag <= 4; Bag++)
+                {
+                    for (Int32 Slot = 1; Slot <= GetContainerNumSlots(Bag); Slot++)
+                    {
+                        String BagItem = GetContainerItemLink(Bag, Slot)[0];
+                        if (BagItem == ItemName)
+                        {
+                            int Count = int.Parse(GetContainerItemInfo(Bag, Slot)[1]);
+                            if(Count > SplitAMount)
+                            {
+                                SplitContainerItem(Bag, Slot, SplitAMount);
+                            }
+
+                        }
+                    }
+                }
+            }
+            catch { }
+        }
+
         private static void SetCurrentGuildBankTab(Int32 n)
         {
             try
@@ -64,6 +88,22 @@ namespace BankLib
         private static int GetContainerNumSlots(Int32 Num)
         {
             return Lua.LuaDoString<int>("return GetContainerNumSlots(" + Num + ");");
+        }
+        
+        private static String[] GetContainerItemInfo(Int32 b, Int32 s)
+        {
+            return Lua.Wow.GetContainerItemInfo<String[]>(b, s);
+        }
+        private static void SplitContainerItem(Int32 bag, Int32 slot, Int32 amount)
+        {
+            try
+            {
+                Lua.LuaDoString("SplitContainerItem("+ bag +"," + slot +","+ amount +")");
+            }
+            finally
+            {
+                Bag.PutSelectedItemInBag();
+            }
         }
 
     }
